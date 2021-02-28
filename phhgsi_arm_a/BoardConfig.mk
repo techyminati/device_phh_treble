c555007cdd41251b218d045e953aba8ba9841503
Aryan Sinha <sinha.aryan03@gmail.com>
2021-02-28 16:36:07 +0530

    Phh-Treble: ARM-A: Decrease System Size So it fits in more devices
    * Xiaomi Redmi Go has System Partition of 1390411776 Bytes , Hence The Large Size GSI's Aint Flash in it & Give Error 255 , Decreasing System May Fix It ! 
    
    Signed-off-by: techyminati <sinha.aryan03@gmail.com>

--- phhgsi_arm_a/BoardConfig.mk
+++ phhgsi_arm_a/BoardConfig.mk
@@ -2,7 +2,7 @@ include build/make/target/board/generic_arm_ab/BoardConfig.mk
 include device/phh/treble/board-base.mk
 
 ifeq ($(BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE),)
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1313583104
+BOARD_SYSTEMIMAGE_PARTITION_SIZE := 1390411776
 else
 BOARD_SYSTEMIMAGE_PARTITION_RESERVED_SIZE := 25165824
 endif
